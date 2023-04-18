a48a8a8f99ce9868d7a91a0dce550d8fe3e97e42
Adam-D-Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>
2023-04-18 17:50:12 -0500

    update

--- nebari_workflow_controller/models.py
+++ nebari_workflow_controller/models.py
@@ -1,11 +1,14 @@
 from typing import List
+
 from pydantic import BaseModel
+
 
 # Models
 class KeycloakGroup(BaseModel):
     id: str
-    name: str 
+    name: str
     path: str
+
 
 class KeycloakUser(BaseModel):
     username: str
