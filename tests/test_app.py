0681e5735ba71b0c22a7bba98e052153c80c0536
Adam-D-Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>
2023-04-18 17:48:04 -0500

    Revert "update"
    This reverts commit 8a84c88f001c9466fcd8b9bd98979d64de27ae6f.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,33 +1,14 @@
 import yaml
 import pytest
-from pathlib import Path
-from nebari_workflow_controller.app import admission_controller
+from nebari_workflow_controller.app import get_username_groups
 
-[(str(p), True) for p in Path('./tests/test_data').glob('*.yaml')]
 
 @pytest.mark.parametrize(
-	"request_file,allowed",
-	[
-		# [(str(p), True) for p in Path('./tests/test_data').glob('*.yaml')]
-		('tests/test_data/request_from_browser.yaml', True), 
-		('tests/test_data/request_from_argo_cli.yaml', True), 
-		('tests/test_data/request_from_kubectl_malicious.yaml', False), 
-		('tests/test_data/request_container_volume_without_subPath.yaml', False), 
-		('tests/test_data/request_like_jupyterlab_pod.yaml', True), 
-		('tests/test_data/request_initContainer_volume_without_subPath.yaml', False)]
+	"request_file",
+	["./tests/test_data/request_from_browser.yaml",]
 )
-def test_admission_controller(mocker, request_file, allowed):
-	mocker.patch('nebari_workflow_controller.app.get_username_groups', 
-		return_value=(
-			'mocked_username', 
-			[
-				{'id': '3135c469-02a9-49bc-9245-f886e6317397', 'name': 'admin', 'path': '/admin'}, 
-				{'id': '137d8913-e7eb-4d68-85a3-59a7a15e99fa', 'name': 'analyst', 'path': '/analyst'}
-			]
-		)
-	)
+def test_admission_controller(request_file, mocker):
+	mocker.patch('app.get_username_groups', lambda request: 'adam', ['nebari'])
 	with open(request_file) as f:
 		request = yaml.load(f, Loader=yaml.FullLoader)
-	response = admission_controller(request)
-	print(response)
-	assert response['response']['allowed'] == allowed
+	print(get_username_groups(request))
